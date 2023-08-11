81edcc96b8ff2e7dfe6ef976d2eba49a3bf3675f
jiayi <jiayizhan2005@gmail.com>
2023-08-11 15:15:03 +0200

    oii_incendio

--- olinfo_training/incendio.cpp
+++ olinfo_training/incendio.cpp
@@ -1,81 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using Grid = vector<vector<bool>>;
+int alzati(int N, int M, int X[], int Y[]) { // time: O(M ^ 2)
+                                             // space: O(M)
+  int dist[M];
+  unordered_set<int> vis;
+  for(int i = 0; i < M; ++i) {
+    vis.insert(i);
+    dist[i] = min(N - Y[i] - 1, X[i]);
+  }
 
-int dx[4] = { -1, 0, 1, 0 }, dy[4] = { 0, 1, 0, -1 };
-int n;
+  while(vis.size()) {
+    int curr = *vis.begin();
+    for(const int& u : vis)
+      if(dist[u] < dist[curr]) curr = u;
 
-bool bfs(Grid& g) {
-  Grid vis = g;
-  if (g[0][0] || g[n - 1][n - 1]) return false;
+    vis.erase(curr);
 
-  queue<pair<int, int>> q;
-  q.emplace(0, 0);
-  vis[0][0] = 1;
-
-  while (!q.empty()) {
-    auto [i, j] = q.front();
-    q.pop();
-
-    for (int l = 0; l < 4; ++l) {
-      int x = i + dx[l], y = j + dy[l];
-      if (x >= 0 && x < n && y >= 0 && y < n && !vis[x][y]) {
-        vis[x][y] = 1;
-        q.emplace(x, y);
-      }
+    for(const int& next : vis) {
+      int dx = abs(X[curr] - X[next]), dy = abs(Y[curr] - Y[next]), 
+        t = (dx == 0 || dy == 0) - 1;
+      int w = (dx + dy + t) / 2, d = max(dist[curr], w);
+      if(d < dist[next]) dist[next] = d;
     }
   }
-  return vis[n - 1][n - 1];
-}
 
-void advance(Grid& g) {
-  vector<int> I, J;
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      if (g[i][j]) {
-        I.push_back(i);
-        J.push_back(j);
-      }
-    }
-  }
-  for (int l = 0; l < I.size(); ++l) {
-    int i = I[l], j = J[l]; 
-    for (int l = 0; l < 4; ++l) {
-      int x = i + dx[l], y = j + dy[l];
-      if (x >= 0 && x < n && y >= 0 && y < n)
-        g[x][y] = 1;
-    }
-  }
-}
-
-int alzati(int N, int M, int X[], int Y[]) {
-  Grid g(N, vector<bool>(N, 0));
-  n = N;
-  for (int i = 0; i < M; ++i) {
-    g[X[i]][Y[i]] = 1;
-  }
-  int ans = 1;
-  advance(g);
-
-  while (bfs(g)) {
-    ++ans;
-    advance(g);
-  }
-
+  int ans = N;
+  for(int i = M - 1; i >= 0; --i) 
+    ans = min(max(dist[i], min(N - X[i] - 1, Y[i])), ans);
   return ans - 1;
 }
 
 int main() {
   int N, M;
-  freopen("error.txt", "w", stderr);
 
   assert(2 == scanf("%d %d", &N, &M));
 
   int* X = (int*)malloc(M * sizeof(int));
   int* Y = (int*)malloc(M * sizeof(int));
 
-  for (int i = 0; i < M; i++)
+  for(int i = 0; i < M; i++)
     assert(2 == scanf("%d %d", X + i, Y + i));
 
   printf("%d\n", alzati(N, M, X, Y));
